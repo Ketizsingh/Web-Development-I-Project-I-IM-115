ee751d76cbc7ca19b6666e4a8e52bd3fb763fc3e
Nikhil Rawal <rawal.nikhil29@gmail.com>
2020-03-27 19:41:22 -0400

    meta viewport tags, comments and tags for mozilla support
    Hey guys,
    I have made a few changes including meta tags for viewport, comments in the file and tags for mozilla support.

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,13 +1,17 @@
 
-function barmove(start,end) {
-
-  var elem = document.getElementById("myBar");   
+function barmove(start,end)                   /* function name barmove */
+{
+  var elem = document.getElementById("myBar");     /* gets element in survey.html using getElementById */  
   var width = start;
   var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= end) {
+  function frame()
+  {
+    if (width >= end)
+    {
       clearInterval(id);
-    } else {
+    }
+    else
+    {
       width++; 
       elem.style.width = width + '%'; 
       elem.innerHTML = width * 1  + '%';
@@ -15,79 +19,62 @@ function barmove(start,end) {
   }
 }
 
-
-
-function next_question(values) {
-
-    if (values == '1') {
-
-
-        document.getElementById("survey-intro-page").style.display = "none";
-
-        document.getElementById("survey-first-five-questions").style.display = "unset";
-
-        document.getElementById("next-question-button").innerHTML = 'Next 5 Questions';
-        document.getElementById("next-question-button").setAttribute("onclick", "next_question(2)");
-
-        barmove(0,30);
-
-    
-
-    }
-    if (values == '2') {
-
-
-        document.getElementById("survey-first-five-questions").style.display = "none";
-
-        document.getElementById("survey-second-five-questions").style.display = "unset";
-
-
-        document.getElementById("next-question-button").innerHTML = 'Last 5 Questions';
-        document.getElementById("next-question-button").setAttribute("onclick", "next_question(3)");
-
-
-        barmove(30,60);
-
-
-
-
-
-    } else if (values == '3') {
-        document.getElementById("survey-second-five-questions").style.display = "none";
-        document.getElementById("survey-second-five-questions").style.display = "none";
-        document.getElementById("survey-third-five-questions").style.display = "unset";
-
-        document.getElementById("next-question-button").innerHTML = 'Submit Survey';
-        document.getElementById("next-question-button").setAttribute("onclick", "finish()");
-
-       barmove(60,100);
-
-
-}
-}
-
-function finish() {
-
-
-    document.getElementById("survey-exit-page").style.display = "unset";
-    document.getElementById("survey-third-five-questions").style.display = "none";
-    document.getElementById("next-question-button").style.display = "none";
-    document.getElementById("progress-bar").style.display = "none";
-    
-
-}
-
-function confirmpassword()
+function next_question(values)            /* function next_question */
 {
-    var display;
-    if(confirm('Your given password will be reset once you confirm your email.'))
-    {
-        display='Congratulations! Your password has been changed';
-    }
-    else
-    {
-        display='Re-enter your password';
-    }
+                                /* gets element in survey.html and display becomes available on questions */
+  if (values == '1')
+  {
+    document.getElementById("survey-intro-page").style.display = "none";
+
+    document.getElementById("survey-first-five-questions").style.display = "unset";
+
+    document.getElementById("next-question-button").innerHTML = 'Next 5 Questions';
+    document.getElementById("next-question-button").setAttribute("onclick", "next_question(2)");
+
+    barmove(0,30);
+  }
+
+  if (values == '2')
+  {
+    document.getElementById("survey-first-five-questions").style.display = "none";
+
+    document.getElementById("survey-second-five-questions").style.display = "unset";
+
+    document.getElementById("next-question-button").innerHTML = 'Last 5 Questions';
+    document.getElementById("next-question-button").setAttribute("onclick", "next_question(3)");
+
+    barmove(30,60);
+  }
+    else if (values == '3')
+  {
+    document.getElementById("survey-second-five-questions").style.display = "none";
+    document.getElementById("survey-second-five-questions").style.display = "none";
+    document.getElementById("survey-third-five-questions").style.display = "unset";
+
+    document.getElementById("next-question-button").innerHTML = 'Submit Survey';
+    document.getElementById("next-question-button").setAttribute("onclick", "finish()");
+
+    barmove(60,100);
+  }
 }
 
+function finish()               /* function finish */
+{
+  document.getElementById("survey-exit-page").style.display = "unset";
+  document.getElementById("survey-third-five-questions").style.display = "none";
+  document.getElementById("next-question-button").style.display = "none";
+  document.getElementById("progress-bar").style.display = "none";  
+}
 
+function confirmpassword()                /* function confirmpassword */
+{
+  var display;
+  if(confirm('Your given password will be reset once you confirm your email.'))
+  {
+    display='Congratulations! Your password has been changed';
+  }
+    else
+  {
+    display='Re-enter your password';
+  }
+}
